87ffaea3c21d4641944129d6d8912f8c3d03e4ae
pietr <pietr@192.168.56.1>
2017-09-12 10:56:57 -0600

    Implemented the amazing class to main method

--- src/controller.java
+++ src/controller.java
@@ -3,7 +3,8 @@ public class controller {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World \n");
-
+		mostAmazingClass amazingness = new mostAmazingClass();
+		amazingness.printAmazingStatement();
 	}
 
 }
--- src/mostAmazingClass.java
+++ src/mostAmazingClass.java
@@ -1,4 +1,7 @@
 
 public class mostAmazingClass {
-
+	public void printAmazingStatement()
+	{
+		System.out.println("Cheers for fancy Wednesday for Software Design!");
+	}
 }
