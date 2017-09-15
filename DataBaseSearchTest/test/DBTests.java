7068567dacb108a5f32d24e096ecef4c8f0cf222
jacobnehama <jacobnehama@172.24.0.37>
2017-09-15 13:18:00 -0600

    Test cases submitted

---
+++ DataBaseSearchTest/test/DBTests.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DBTestss {
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testDBLength() { // test to see if pile total is more than 31 
+		
+	}
+	@Test
+	public void testDB() { // test to see if pile total is more than 31 
+
+	}
+	@Test
+	public void testToBeImplemented() {
+	}
+	
+
+}
