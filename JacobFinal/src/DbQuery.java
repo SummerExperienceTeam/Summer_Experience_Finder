2f7c24d9d048c13036c0491a63dbcbca45642486
jacobnehama <jacobnehama@172.24.0.37>
2017-09-14 20:24:46 -0600

    jacob commit

--- JacobFinal/src/DbQuery.java
+++ JacobFinal/src/DbQuery.java
@@ -31,7 +31,7 @@ public class DbQuery extends JFrame {
 		
 		setTitle("Database Search");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(500, 500, 900, 600);
+		setBounds(400, 400, 900, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(50, 50, 50, 50));
 		contentPane.setLayout(new BorderLayout(0, 0));
