c7c68e6770ce3e2dec24df667a00ba70b091edd2
jacobnehama <jacobnehama@172.24.0.37>
2017-09-14 20:15:57 -0600

    first

---
+++ JacobFinal/src/DbQuery.java
@@ -0,0 +1,77 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListCellRenderer;
+
+
+public class DbQuery extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField lastNameTextField;
+	private JButton btnSearch;
+	private JScrollPane scrollPane;
+	private JTable table;
+	private JComboBox filterList;
+	
+
+	public static void main(String[] args) {
+		DbQuery ourGUI = new DbQuery();
+	}
+
+	
+	public DbQuery() {
+		
+		setTitle("Database Search");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(200, 200, 900, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(50, 50, 50, 50));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane); 
+		
+		
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		
+		//JLabel enterQuery = new JLabel("Enter Query");
+		//panel.add(enterQuery);
+		
+		//lastNameTextField = new JTextField();
+		//panel.add(lastNameTextField);
+		//lastNameTextField.setColumns(10);
+		
+		//btnSearch = new JButton("Search");
+		//panel.add(btnSearch);
+		
+		scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+	
+	
+		
+		String[] filterNames = { "Select State", "Alabama", "Alaska", "Arkansas", "California" };
+		JComboBox filterList = new JComboBox(filterNames);
+		panel.add(filterList);
+		
+
+		setVisible(true);
+		
+		
+	}
+
+}
